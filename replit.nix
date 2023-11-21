503e227ea98144491066eb4c8d584b4f4b8348f8
clevvbotz <108448307+clevvbotz@users.noreply.github.com>
2023-11-22 05:22:16 +0800

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -4,13 +4,14 @@
         pkgs.nodejs-16_x
         pkgs.libwebp
         pkgs.python
-	      pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript
         pkgs.libuuid
         pkgs.ffmpeg
         pkgs.imagemagick  
         pkgs.wget
         pkgs.git
         pkgs.nodePackages.pm2
+        pkgs.yarn
     ];
   env ={
     LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
